a361477f62e038030a11bba1ce9822333b69e00b
JGevers <31844777+JGevers@users.noreply.github.com>
2017-10-06 10:31:49 +0200

    Update SoftwareEngineering.java

--- ProjectQ/app/src/main/java/com/teamawesome/projectq/SoftwareEngineering.java
+++ ProjectQ/app/src/main/java/com/teamawesome/projectq/SoftwareEngineering.java
@@ -1,13 +1,43 @@
 package com.teamawesome.projectq;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
 
 public class SoftwareEngineering extends AppCompatActivity {
+
+    private static final String TAG = "SoftwareEngineering";
+
+    private WebView mWebView;
+    private Button mjobBTN1;
+    private Button mjobBTN2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_software_engineering);
+
+        mjobBTN1 = (Button) findViewById(R.id.jobBTN1);
+
+        mjobBTN1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=vt79JcPfZQA"));
+                startActivity(intent1);
+            }
+        });
+        mjobBTN2 = (Button) findViewById(R.id.jobBTN2);
+
+        mjobBTN2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=2AnVUaiEnZk"));
+                startActivity(intent2);
+            }
+        });
     }
 }

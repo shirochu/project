882fdd8e29f46b2c62f1f933c7da743c2795af03
JGevers <31844777+JGevers@users.noreply.github.com>
2017-10-06 11:49:03 +0200

    Update ITSM.java

--- ProjectQ/app/src/main/java/com/teamawesome/projectq/ITSM.java
+++ ProjectQ/app/src/main/java/com/teamawesome/projectq/ITSM.java
@@ -1,13 +1,41 @@
 package com.teamawesome.projectq;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
 
 public class ITSM extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_itsm);
+        private WebView mWebView;
+        private Button mjobBTN1;
+        private Button mjobBTN2;
+
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_itsm);
+
+            mjobBTN1 = (Button) findViewById(R.id.jobBTN1);
+
+            mjobBTN1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=oqGWP1YG7QA"));
+                    startActivity(intent1);
+                }
+            });
+            mjobBTN2 = (Button) findViewById(R.id.jobBTN2);
+
+            mjobBTN2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=_dAomtQ9Oxg"));
+                    startActivity(intent2);
+                }
+            });
+        }
     }
-}

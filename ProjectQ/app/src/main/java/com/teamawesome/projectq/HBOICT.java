fd6bac7337d82ee433ae66d995601ca6bd889372
JGevers <31844777+JGevers@users.noreply.github.com>
2017-11-03 14:10:28 +0100

    Update HBOICT.java

--- ProjectQ/app/src/main/java/com/teamawesome/projectq/HBOICT.java
+++ ProjectQ/app/src/main/java/com/teamawesome/projectq/HBOICT.java
@@ -18,6 +18,10 @@ public class HBOICT extends AppCompatActivity {
         Button navITSM = (Button) findViewById(R.id.itsmBtn);
         Button navSE = (Button) findViewById(R.id.seBtn);
         Button navSAX = (Button) findViewById(R.id.saxionBtn);
+        Button Site1 = (Button) findViewById(R.id.Site1);
+        Button Site2 = (Button) findViewById(R.id.Site2);
+        Button Site3 = (Button) findViewById(R.id.Site3);
+
 
         // functie knop Business
         navBusiness.setOnClickListener(new View.OnClickListener() {
@@ -49,13 +53,38 @@ public class HBOICT extends AppCompatActivity {
             }
         });
 
-        // functie knop SE
+        // functie knop Sax
         navSAX.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Intent intent4 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.saxion.nl/studeren/kiezen_en_kennismaken/Studiekiezer/act/hbo-ict/hbo-ict/"));
                 startActivity(intent4);
             }
         });
+
+        Site1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent5 = new Intent(Intent.ACTION_VIEW, Uri.parse("http://vanbeers.ddns.net/saxion/posb/index.html" ));
+                startActivity(intent5);
+            }
+        });
+
+        Site2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent6 = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.hbo-ict.tk/" ));
+                startActivity(intent6);
+            }
+        });
+
+        Site3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent7 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=P1TciR0GhZw" ));
+                startActivity(intent7);
+            }
+        });
+
     }
 }
